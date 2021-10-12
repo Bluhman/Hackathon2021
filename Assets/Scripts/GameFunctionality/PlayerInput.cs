f7fef8601f4c6665512f86f533d9828929bfdf86
Micah Fujiwara <mifujiwa@microsoft.com>
2021-10-12 12:24:17 -0700

    Fixing up some input stuff so that Geoff can work on death-state and menu components.

--- Assets/Scripts/GameFunctionality/PlayerInput.cs
+++ Assets/Scripts/GameFunctionality/PlayerInput.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerController))]
 public class PlayerInput : MonoBehaviour
 {
-
 	PlayerController player;
+	public float doubleTapDashTimingWindow = 0.25f;
+	private bool _pressedDirectionRecently; 
 
 	void Start()
 	{
@@ -18,6 +19,11 @@ public class PlayerInput : MonoBehaviour
 
 		Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		player.SetDirectionalInput(directionalInput);
+
+		if (!_pressedDirectionRecently)
+		{
+			_pressedDirectionRecently = directionalInput.magnitude >= 1;
+		}
 
 		if (Input.GetButtonDown("Jump"))
 		{
